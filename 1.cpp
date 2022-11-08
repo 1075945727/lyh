0248f77666d344d328cc1593c8f0f0ef9d816415
1075945727 <117559242+1075945727@users.noreply.github.com>
2022-11-08 11:35:51 +0800

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,62 @@
+#include< stdio.h>
+#include< ctype.h>
+/*
+int main (void)
+{
+    char ch;
+    printf("给我一个字母, 我会给出一个以该字母开头的动物名.\n");
+    printf("请输入一个字母, 键入#退出.\n");
+    while((ch = getchar()) != '#')
+    {
+        if('\n' == ch)
+            continue;
+        if(islower(ch))
+        {
+			if('\n' == ch)
+            continue;
+            switch(ch)
+            {
+                case 'a':
+                    printf("argali, (大野角羊)一种亚洲野羊.\n");
+                    break;
+                case 'b':
+                    printf("babirusa, 马来野猪.\n");
+                    break;
+                case 'c':
+                    printf("coati, 南美浣熊, 像浣熊的哺乳动物.\n");
+                    break;
+                case 'd':
+                    printf("desman, 食虫水栖鼹鼠, 像鼹鼠的小动物.\n");
+                    break;
+                case 'e':
+                    printf("echidna, 针食蚁兽, 有刺的食蚁兽.\n");
+                    break;
+                case 'f':
+                    printf("fisher, 渔貂, 近棕色的貂.\n");
+                default:
+                    printf("是个难题!\n");
+            }
+        }
+        else
+            printf("我只认得小写字母.\n");
+        
+           
+        printf("请输入下一个字母, 或者键入#退出.\n");
+    }
+    printf("Bye!\n");
+	getchar();
+    return 0;
+}
+*/
+
+int main()
+{
+	printf("%Hello world!\n");
+}
+
+
+
+
+
+
+	
